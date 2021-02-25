b9fd6fd7916a8e864472cb96f1cd24d3a8f3b173
Johan <johan-avila@outlook.com>
2021-02-25 12:46:35 -0600

    Delete user

--- src/utils/middlewares/validateIdAndSub.js
+++ src/utils/middlewares/validateIdAndSub.js
@@ -3,7 +3,6 @@ const boom = require('@hapi/boom');
 module.exports = function validateIdAndSub() {
     return function middleware(req, res, next) {
         try {
-            console.log('__req.userData___');
             if (req.userData.sub !== req.params.id) {
                 throw boom.unauthorized('Invalid user id 🐻');
             }
