e8a39d92b10c8122c763e7e4034acacdcde75ccb
Alexander herrera <chander.zap@gmail.com>
2021-02-26 00:12:23 -0600

    [ADD] add and search

--- src/api/components/elasticSearch/controller.js
+++ src/api/components/elasticSearch/controller.js
@@ -1,11 +1,70 @@
 const client = require('../../../utils/connections/elasticSearch');
+const boom = require('@hapi/boom');
 
 
 const chechHealth = () => {
     // client.cluster.health();
     return client.ping({});
 }
+const createIndex = (index) => {
+    return client.indices.create({
+        index: index
+    })
+}
+const insertDocument = (index, headline, subhead, article) => {
+    try {
+        return client.index({
+            index: index,
+            body: {
+                headline,
+                subhead,
+                article
+            }
+        });
+    } catch (error) {
+        throw boom.badRequest(error);
+    }
+}
+
+const refreshIndex = (index) => {
+    try {
+        return client.indices.refresh({ index: index });
+    } catch (error) {
+        throw boom.internal(error);
+    }
+}
+
+const searchBasic = async (index, lookingfor) => {
+    try {
+        const res = await client.search({
+            index: index,
+            // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+            body: {
+                query: {
+                    simple_query_string: {
+                        query: lookingfor
+                    }
+                }
+            }
+        })
+
+        return res;
+    } catch (error) {
+        throw boom.badRequest(error)
+    }
+}
+
+const getMappings = async (index) => {
+    return client.indices.getMapping({
+        index
+    })
+}
 
 module.exports = {
-    chechHealth
+    chechHealth,
+    createIndex,
+    insertDocument,
+    refreshIndex,
+    searchBasic,
+    getMappings
 }
