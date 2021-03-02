956f1a74c4fddc9c2dcb0d7c93acf21cc42192a1
Johan <johan-avila@outlook.com>
2021-03-01 21:57:34 -0600

    Test Auth Users

--- src/utils/testServer.js
+++ src/utils/testServer.js
@@ -2,16 +2,16 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const supertest = require('supertest');
 
+const app = express();
 function testServer(path, router) {
-    const app = express();
     //These middlewares required for testing
     app.use(express.json());
     app.use(cookieParser());
     app.use(express.urlencoded({ extended: true }));
 
-    app.use(`${path}`,router)
+    app.use(path,router)
 
     return supertest(app)
 }
 
-module.exports = testServer
+module.exports = {testServer}
