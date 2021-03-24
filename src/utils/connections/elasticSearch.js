036d0ae87c80b2f7b44d02aca33c29c054903f49
Alexander herrera <chander.zap@gmail.com>
2021-03-24 12:15:15 -0600

    [FIX] ES url

--- src/utils/connections/elasticSearch.js
+++ src/utils/connections/elasticSearch.js
@@ -2,8 +2,7 @@ const { config } = require('../../config/index');
 const { Client } = require('@elastic/elasticsearch')
 
 
-const url = `https://${config.es_user}:${config.es_pass}@search-searchercoldummy-g6tifm7x4ckvfhs2433rfpb7bu.us-west-1.es.amazonaws.com/`;
-
+const url = `https://${config.es_user}:${config.es_pass}@${config.es_url}`;
 const client = new Client({
   node: url
 })
